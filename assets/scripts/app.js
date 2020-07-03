fc1efa7af473d17fdf3a9a7a060b18abc33548da
bhanukirant99 <bhanukirant99@gmail.com>
2020-07-03 22:49:53 +0530

    first

---
+++ assets/scripts/app.js
@@ -0,0 +1,82 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "css3",
+				weight: 8
+			}, {
+				text: "javascript",
+				weight: 14
+			}, {
+				text: "jquery",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "python",
+				weight: 10
+			}, {
+				text: "java",
+				weight: 9
+			}, {
+				text: "bhanukiran",
+				weight: 15
+			}, {
+				text: "C",
+				weight: 7
+			},{
+				text: "Cyber Security",
+				weight: 7
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Full-Stack Web Developer.", "love everything about code.", "solve problems."
+			],
+			stringsElement: null,
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
